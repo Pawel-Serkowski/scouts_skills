1aef691cff47684cafbdd974f28729532d3dad3e
Pawel-Serkowski <paw.serkowski@gmail.com>
2023-01-11 20:54:31 +0100

    yeah, good

--- src/SkillsPage/Components/SkillsViewer.js
+++ src/SkillsPage/Components/SkillsViewer.js
@@ -69,36 +69,8 @@ const SkillsViewer = (props) =>{
                     </section>
                 </div>
 
-                {/* <section className={`mobile-viewer ${props.isActive ? "active" : ""}`}>
-                     <div className="skills-viewer__cross" onClick={props.action}>
-                    <i class="fa-solid fa-arrow-left"></i>
-                    </div>
-                    <div className='skills-viewer__title'>
-                            <h2>{nazwa[0]}</h2>
-                            {nazwa[1] ? <h2>{nazwa[1]}</h2> : <p></p>}
-                    </div>
-                    <div className='skills-viewer__level'>
-                        <h2>{props.skill.trudnosc}</h2>
-                    </div>
-                    <section className="skills-viewer__tasks">
-                        <h1>Zadania:</h1>
-                        {props.skill.zadania.map(task =>
-                        <div className='skills-viewer__task'>
-                            {task_creator(task).map(tasky => 
-                                <p className={!tasky.includes("$") ? "under" : "main"}>{tasky.split("$")[0]}</p>
-                            )}
-                        </div>
-                        )}
-                    </section>
-                    <section className="skills-viewer__description">
-                        <div className="img-box">
-                                <img src={require(`../../shared${props.skill.img}`)} alt=""/>
-                        </div>
-                        <p className="skills-viewer__idea">{props.skill.idea}</p>
-                    </section>
-                </section> */}
                         
-                </React.Fragment>:   <h1>Nothing choosed</h1> }
+                </React.Fragment>:   <h1 className="skills-viewer__nothing">nothing is selected</h1> }
                 </section>
             
         
