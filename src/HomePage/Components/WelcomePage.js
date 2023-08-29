ae244652f33bd178868c9340b96616ddc3d2b242
Pawel-Serkowski <paw.serkowski@gmail.com>
2023-08-29 23:06:21 +0200

    #1HomePage changed: button style and background on the HomePage. New idea - try to split screen in half and do smooth transition

--- src/HomePage/Components/WelcomePage.js
+++ src/HomePage/Components/WelcomePage.js
@@ -1,22 +1,24 @@
-import React from 'react';
+import React from "react";
 
-import Button from '../../shared/forms/Button/Button';
-import "./WelcomePage.css"
+import Button from "../../shared/forms/Button/Button";
+import "./WelcomePage.css";
 
-const WelcomePage = () =>{
-
-    return(
-        <section className="welcome-page__container">
-            <div className="empty"></div>
-            <div className="organizer">
-                <h1>Poszukujesz sprawności dla siebie? <br></br>
-                    Sprawdź wśród około 481 wyzwań. Może któreś akurat dla Ciebie!
-                </h1>
-                <Button to="/sprawnosci" active_text="dark" styles="lg " >Sprawdź</Button>
-            </div>
-            {/* <SmoothScroll to={"TypesPage"} time={500}/> */}
-        </section>
-    )
+const WelcomePage = () => {
+  return (
+    <section className="welcome-page__container">
+      <div className="empty"></div>
+      <div className="organizer">
+        <h1>
+          Poszukujesz sprawności dla siebie? <br></br>
+          Sprawdź wśród około 481 wyzwań. Może któreś akurat dla Ciebie!
+        </h1>
+        <Button to="/sprawnosci" active_text="dark" styles="lg up-slide">
+          Sprawdź
+        </Button>
+      </div>
+      {/* <SmoothScroll to={"TypesPage"} time={500}/> */}
+    </section>
+  );
 };
 
 export default WelcomePage;
