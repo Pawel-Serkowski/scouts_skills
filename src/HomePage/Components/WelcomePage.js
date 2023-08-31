d490619454c0abc422cedfb7936da69fc99923ec
pserkows <pawel_serkowski@sevenet.pl>
2023-08-31 10:26:59 +0200

    added new look of main page

--- src/HomePage/Components/WelcomePage.js
+++ src/HomePage/Components/WelcomePage.js
@@ -3,15 +3,41 @@ import React from "react";
 import Button from "../../shared/forms/Button/Button";
 import "./WelcomePage.css";
 
+import CircleImage from "../../shared/img/circle.svg";
+import TriangleImage from "../../shared/img/triangle.svg";
+
 const WelcomePage = () => {
    return (
       <section className="welcome-page__container">
-         <div className="organizer organizer1">
-            <div className="layout"></div>
-         </div>
-         <div className="organizer organizer2">
-            <div className="layout"></div>
-         </div>
+         <header className="welcome-page__heading">
+            <h1>Tropy</h1>
+            <div className="quote_block">
+               <h3 className="quote_text">
+                  <q>Współpraca to siła napędowa wielkich osiągnięć.</q>
+               </h3>
+               <span className="quote_author"> ~John C. Maxwell</span>
+            </div>
+            <div className="shape_icons">
+               {/* <img src={CircleImage} alt="Sprawności Harcerskie" /> */}
+               <img src={TriangleImage} alt="Tropy Zuchowe" />
+            </div>
+         </header>
+         <header className="welcome-page__heading">
+            <h1>Sprawności</h1>
+            <div className="quote_block">
+               <h3 className="quote_text">
+                  <q>
+                     Rozwój to nie końcowy cel, lecz nieustający proces
+                     doskonalenia siebie.
+                  </q>
+               </h3>
+               <span className="quote_author"> ~Tony Robbins</span>
+            </div>
+            <div className="shape_icons">
+               <img src={CircleImage} alt="Sprawności Harcerskie" />
+               <img src={TriangleImage} alt="Sprawności Zuchowe" />
+            </div>
+         </header>
       </section>
    );
 };
